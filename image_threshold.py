c22ba5da799a9dbea36fee16e2dbb3dacd5a4752
Seje Nikolaj xP <npeter17@student.aau.dk>
2018-11-19 10:43:39 +0100

    Regions applied
    Yo

--- image_threshold.py
+++ image_threshold.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-
 
 img1 = cv2.imread('1.png',0)
 img2 = cv2.imread('2.png',0)
@@ -15,14 +13,5 @@ def mask(image):
     return image
 
 
-'''cv2.imshow('mask', mask)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
 
 
-
-#cv2.imshow('Color Detect', img)
-#cv2.imshow('Detection', mask)
-
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
